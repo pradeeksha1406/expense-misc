338ad64dd7d683353fe6a9ed28e938cc28151ce5
pradeep <p.guta@gmail.com>
2024-04-16 14:18:47 +0530

    jenkins file test

---
+++ jenkins-ip/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-b75"
+    key    = "misc/jenkins-ip/terraform.tfstate"
+    region = "us-east-1"
+
+  }
+}
+
+data "aws_instance" "instance" {
+  instance_id = "i-0e33dca48d371c58d"
+}
+
+resource "aws_route53_record" "jenkins" {
+  name    = "jenkins.techdda.co"
+  type    = "A"
+  zone_id = "Z02181513KJGBXLJM7332"
+  records = [data.aws_instance.instance.public_ip]
+  ttl     = 10
+}
+
+
+data "aws_instance" "artifactory" {
+  instance_id = "i-0e33dca48d371c58d"
+}
+
+resource "aws_route53_record" "artifactory" {
+  name    = "artifactory.techadda.co"
+  type    = "A"
+  zone_id = "Z02181513KJGBXLJM7332"
+  records = [data.aws_instance.artifactory.public_ip]
+  ttl     = 10
+}
