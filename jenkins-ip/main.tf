6c8b3d0b7942a2693058578d8d946c30d393cad5
pradeep <p.guta@gmail.com>
2024-05-07 17:18:20 +0530

    pipeline

--- jenkins-ip/main.tf
+++ jenkins-ip/main.tf
@@ -20,14 +20,14 @@ resource "aws_route53_record" "jenkins" {
 }
 
 
-#data "aws_instance" "artifactory" {
-#  instance_id = "i-0e33dca48d371c58d"
-#}
-#
-#resource "aws_route53_record" "artifactory" {
-#  name    = "artifactory.techadda.co"
-#  type    = "A"
-#  zone_id = "Z02181513KJGBXLJM7332"
-#  records = [data.aws_instance.artifactory.public_ip]
-#  ttl     = 10
-#}
+data "aws_instance" "artifactory" {
+  instance_id = "i-0e33dca48d371c58d"
+}
+
+resource "aws_route53_record" "artifactory" {
+  name    = "artifactory.techadda.co"
+  type    = "A"
+  zone_id = "Z02181513KJGBXLJM7332"
+  records = [data.aws_instance.artifactory.public_ip]
+  ttl     = 10
+}
